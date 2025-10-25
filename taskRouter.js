da87d58a6ff70cd83f5a299902dab960f7a109d7
ArulRajan35 <230701035@rajalakshmi.edu.in>
2025-10-25 22:54:13 +0530

    taskRouter.js

---
+++ taskRouter.js
@@ -0,0 +1,11 @@
+### routers/taskRouter.js
+```javascript
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+router.post('/tasks', taskController.createTask);
+router.get('/tasks', taskController.getTasks);
+
+module.exports = router;
+```
